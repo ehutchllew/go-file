257999ad495b356bcbaf5c5f6d625d9b6fb67c2b
Evan Hutch <ehutch7@gmail.com>
2023-07-27 22:11:46 +0000

    Making tons of changes to ensure data integrity with out-of-order columns

--- cmd/root.go
+++ cmd/root.go
@@ -15,12 +15,113 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
+const (
+	SAT_ACCT           = "Sat Acct"
+	BILL_STATUS        = "Bill Status"
+	HOST_BILL_FROM     = "Host Bill From"
+	HOST_BILL_TO       = "Host Bill To"
+	TRANSFERRED_KWH    = "Transferred kWh"
+	BANKED_PRIOR_MONTH = "Banked Prior Month"
+	ALLOCATION_PERCENT = "Allocation %"
+	APPLIED            = "Applied"
+	BANKED_CARRY_OVER  = "Banked Carry Over"
+)
+
+type ColumnIndex struct {
+	FileName           string
+	SAT_ACCT           *int
+	BILL_STATUS        *int
+	HOST_BILL_FROM     *int
+	HOST_BILL_TO       *int
+	TRANSFERRED_KWH    *int
+	BANKED_PRIOR_MONTH *int
+	ALLOCATION_PERCENT *int
+	APPLIED            *int
+	BANKED_CARRY_OVER  *int
+}
+
+func (c *ColumnIndex) GetFileHeaders() []string {
+	fileHeaders := []string{
+		SAT_ACCT,
+		BILL_STATUS,
+		HOST_BILL_FROM,
+		HOST_BILL_TO,
+		TRANSFERRED_KWH,
+		BANKED_PRIOR_MONTH,
+		ALLOCATION_PERCENT,
+		APPLIED,
+		BANKED_CARRY_OVER,
+	}
+
+	return fileHeaders
+}
+
+func (c *ColumnIndex) SortWritableData(data *[]string) []string {
+	dataValues := *data
+	return []string{
+		dataValues[*c.SAT_ACCT],
+		dataValues[*c.BILL_STATUS],
+		dataValues[*c.HOST_BILL_FROM],
+		dataValues[*c.HOST_BILL_TO],
+		dataValues[*c.TRANSFERRED_KWH],
+		dataValues[*c.BANKED_PRIOR_MONTH],
+		dataValues[*c.ALLOCATION_PERCENT],
+		dataValues[*c.APPLIED],
+		dataValues[*c.BANKED_CARRY_OVER],
+	}
+}
+
+func (c *ColumnIndex) WhichFieldsExist() map[string]string {
+	existMap := map[string]string{
+		SAT_ACCT:           "false",
+		BILL_STATUS:        "false",
+		HOST_BILL_FROM:     "false",
+		HOST_BILL_TO:       "false",
+		TRANSFERRED_KWH:    "false",
+		BANKED_PRIOR_MONTH: "false",
+		ALLOCATION_PERCENT: "false",
+		APPLIED:            "false",
+		BANKED_CARRY_OVER:  "false",
+	}
+
+	if c.SAT_ACCT != nil {
+		existMap[SAT_ACCT] = "true"
+	}
+	if c.BILL_STATUS != nil {
+		existMap[BILL_STATUS] = "true"
+	}
+	if c.HOST_BILL_FROM != nil {
+		existMap[HOST_BILL_FROM] = "true"
+	}
+	if c.HOST_BILL_TO != nil {
+		existMap[HOST_BILL_TO] = "true"
+	}
+	if c.TRANSFERRED_KWH != nil {
+		existMap[TRANSFERRED_KWH] = "true"
+	}
+	if c.BANKED_PRIOR_MONTH != nil {
+		existMap[BANKED_PRIOR_MONTH] = "true"
+	}
+	if c.ALLOCATION_PERCENT != nil {
+		existMap[ALLOCATION_PERCENT] = "true"
+	}
+	if c.APPLIED != nil {
+		existMap[APPLIED] = "true"
+	}
+	if c.BANKED_CARRY_OVER != nil {
+		existMap[BANKED_CARRY_OVER] = "true"
+	}
+
+	return existMap
+}
+
 var (
-	aggFile        *csv.Writer
-	containsHeader bool
-	mu             sync.Mutex
-	wg             sync.WaitGroup
-	validationFile *csv.Writer
+	aggFile                      *csv.Writer
+	containsValidationFileHeader bool
+	containsAggFileHeader        bool
+	mu                           sync.Mutex
+	wg                           sync.WaitGroup
+	validationFile               *csv.Writer
 )
 
 var (
@@ -70,7 +171,6 @@ var (
 				}
 
 				if !d.IsDir() && filepath.Ext(path) == ".xlsx" {
-					fmt.Println("wg add parseExcelFile")
 					wg.Add(1)
 					go parseExcelFile(path, &ch)
 				}
@@ -108,16 +208,19 @@ func init() {
 }
 
 func parseExcelFile(filePath string, ch *chan []string) {
-	defer func() {
-		fmt.Println("defer parseExcelFile")
-		wg.Done()
-	}()
+	defer wg.Done()
 
 	file, err := excelize.OpenFile(filePath)
 	if err != nil {
 		log.Fatalf("Cannot open file (%s)\n Err: %v", filePath, err)
 	}
 	defer file.Close()
+
+	fileName := filepath.Base(filePath)
+
+	fileColumnIndexMap := ColumnIndex{
+		FileName: fileName,
+	}
 
 	sheets := file.GetSheetList()
 	for _, sheet := range sheets {
@@ -127,27 +230,54 @@ func parseExcelFile(filePath string, ch *chan []string) {
 		}
 
 		for i, row := range rows {
-			// TODO: sanitize row
-			sanitizeRow(row)
 			if i == 0 {
-				validateFile(filepath.Base(filePath), row)
-				fmt.Println("wg add writeToAggFile")
-				wg.Add(1)
-				writeToAggFile(&row)
+				populateIndexMap(row, &fileColumnIndexMap)
+				validateFile(&fileColumnIndexMap)
+
+				if !containsAggFileHeader {
+					fileHeaders := fileColumnIndexMap.GetFileHeaders()
+					wg.Add(1)
+					writeToAggFile(&fileHeaders)
+					containsAggFileHeader = true
+				}
 			} else {
-				fmt.Println("wg add writeToAggFile")
 				wg.Add(1)
-				*ch <- row
+				*ch <- fileColumnIndexMap.SortWritableData(&row)
 			}
 		}
 
 	}
 }
 
+func populateIndexMap(headers []string, fileColumnIndexMap *ColumnIndex) error {
+	for i, header := range headers {
+		lowerHeader := strings.ToLower(header)
+		switch lowerHeader {
+		case strings.ToLower(SAT_ACCT):
+			fileColumnIndexMap.SAT_ACCT = &i
+		case strings.ToLower(BILL_STATUS):
+			fileColumnIndexMap.BILL_STATUS = &i
+		case strings.ToLower(HOST_BILL_FROM):
+			fileColumnIndexMap.HOST_BILL_FROM = &i
+		case strings.ToLower(HOST_BILL_TO):
+			fileColumnIndexMap.HOST_BILL_TO = &i
+		case strings.ToLower(TRANSFERRED_KWH):
+			fileColumnIndexMap.TRANSFERRED_KWH = &i
+		case strings.ToLower(BANKED_PRIOR_MONTH):
+			fileColumnIndexMap.BANKED_PRIOR_MONTH = &i
+		case strings.ToLower(ALLOCATION_PERCENT):
+			fileColumnIndexMap.ALLOCATION_PERCENT = &i
+		case strings.ToLower(APPLIED):
+			fileColumnIndexMap.APPLIED = &i
+		case strings.ToLower(BANKED_CARRY_OVER):
+			fileColumnIndexMap.BANKED_CARRY_OVER = &i
+		}
+	}
+
+	return nil
+}
+
 func readFromChannel(ch *chan []string, done *chan int8) {
-	defer func() {
-		fmt.Println("defer readFromChannel")
-	}()
 	for {
 		data, ok := <-*ch
 		if !ok {
@@ -161,10 +291,6 @@ func readFromChannel(ch *chan []string, done *chan int8) {
 	}
 }
 
-func sanitizeRow(row []string) []string {
-	return row
-}
-
 func setupWriteFile(path string) (*csv.Writer, error) {
 	createdFile, err := os.Create(path)
 	if err != nil {
@@ -174,102 +300,34 @@ func setupWriteFile(path string) (*csv.Writer, error) {
 	return csv.NewWriter(createdFile), nil
 }
 
-func validateFile(fileName string, headers []string) {
-	requiredFields := map[string]string{
-		"Sat Acct":           "false",
-		"Bill Status":        "false",
-		"Host Bill From":     "false",
-		"Host Bill To":       "false",
-		"Transferred kWh":    "false",
-		"Banked Prior Month": "false",
-		"Allocation %":       "false",
-		"Applied":            "false",
-		"Banked Carry Over":  "false",
-	}
-
-	for i, header := range headers {
-		if _, ok := requiredFields[header]; ok {
-			switch strings.ToLower(header) {
-			case "sat acct":
-				if i == 1 {
-					requiredFields[header] = "true"
-				} else {
-					requiredFields[header] = "out of order"
-				}
-			case "transferred kwh":
-				if i == 14 {
-					requiredFields[header] = "true"
-				} else {
-					requiredFields[header] = "out of order"
-				}
-			case "allocation %":
-				if i == 15 {
-					requiredFields[header] = "true"
-				} else {
-					requiredFields[header] = "out of order"
-				}
-			case "banked prior month":
-				if i == 19 {
-					requiredFields[header] = "true"
-				} else {
-					requiredFields[header] = "out of order"
-				}
-			case "applied":
-				if i == 23 {
-					requiredFields[header] = "true"
-				} else {
-					requiredFields[header] = "out of order"
-				}
-			case "banked carry over":
-				if i == 24 {
-					requiredFields[header] = "true"
-				} else {
-					requiredFields[header] = "out of order"
-				}
-			}
-		}
-
-	}
-
-	if !containsHeader {
+func validateFile(fileColumnIndexMap *ColumnIndex) {
+	requiredFields := fileColumnIndexMap.WhichFieldsExist()
+	if !containsValidationFileHeader {
 		validationFileHeaders := append([]string{"File Name"},
-			"Sat Acct",
-			"Bill Status",
-			"Host Bill From",
-			"Host Bill To",
-			"Transferred kWh",
-			"Banked Prior Month",
-			"Allocation %",
-			"Applied",
-			"Banked Carry Over",
+			fileColumnIndexMap.GetFileHeaders()...,
 		)
-		fmt.Println("wg add writeToValidationFile")
 		wg.Add(1)
-		containsHeader = true
+		containsValidationFileHeader = true
 		writeToValidationFile(&validationFileHeaders)
 	}
 
-	fmt.Println("wg add writeToValidationFile")
 	wg.Add(1)
 	writeToValidationFile(&[]string{
-		fileName,
-		requiredFields["Sat Acct"],
-		requiredFields["Bill Status"],
-		requiredFields["Host Bill From"],
-		requiredFields["Host Bill To"],
-		requiredFields["Transferred kWh"],
-		requiredFields["Banked Prior Month"],
-		requiredFields["Allocation %"],
-		requiredFields["Applied"],
-		requiredFields["Banked Carry Over"],
+		fileColumnIndexMap.FileName,
+		requiredFields[SAT_ACCT],
+		requiredFields[BILL_STATUS],
+		requiredFields[HOST_BILL_FROM],
+		requiredFields[HOST_BILL_TO],
+		requiredFields[TRANSFERRED_KWH],
+		requiredFields[BANKED_PRIOR_MONTH],
+		requiredFields[ALLOCATION_PERCENT],
+		requiredFields[APPLIED],
+		requiredFields[BANKED_CARRY_OVER],
 	})
 }
 
 func writeToAggFile(data *[]string) {
-	defer func() {
-		fmt.Println("defer writeToAggFile")
-		wg.Done()
-	}()
+	defer wg.Done()
 	// fmt.Printf("\n:::Writing agg data::: %v", *data)
 	mu.Lock()
 	err := aggFile.Write(*data)
@@ -280,10 +338,7 @@ func writeToAggFile(data *[]string) {
 }
 
 func writeToValidationFile(data *[]string) {
-	defer func() {
-		fmt.Println("defer writeToValidationFile")
-		wg.Done()
-	}()
+	defer wg.Done()
 	// fmt.Printf("\n:::Writing validation data::: %v", *data)
 	mu.Lock()
 	err := validationFile.Write(*data)
